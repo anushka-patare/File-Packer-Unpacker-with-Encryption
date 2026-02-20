9d5b558519d939507f25b16f153daf79bf9c57a3
Anushka Patare <anupatare07@gmail.com>
2026-02-20 13:44:09 +0530

    Add files via upload

---
+++ FilePacker57.java
@@ -0,0 +1,119 @@
+// Unpacking code
+
+import java.io.*;
+import java.util.*;
+
+class program613
+{
+    public static void main(String A[]) throws Exception
+    {
+        // Variable creation
+        Scanner sobj = null;
+       
+        int FileSize = 0;
+        int i = 0;
+
+        byte key = 0x11;
+
+        String FileName = null;
+        String Header = null;
+        String Tokens[] = null;
+
+        File fpackobj = null;
+        FileInputStream fiobj = null;
+        FileOutputStream foobj = null;
+        File fobj = null; 
+
+        byte bHeader[] = new byte[100];
+        byte Buffer[] = null;
+       
+        sobj = new Scanner(System.in);
+     
+        System.out.println("Enter the name of packed file:");
+        FileName = sobj.nextLine();
+
+        fpackobj= new File(FileName);
+
+        if( fpackobj.exists() == false)
+        {
+            System.out.println("Error : There is no such packed file");
+            return;
+        }
+
+        fiobj = new FileInputStream(fpackobj);     // Its for reading
+
+        // Read the header
+        fiobj.read(bHeader,0,100);
+
+        Header = new String(bHeader);
+
+        Header =  Header.trim();
+
+        Tokens = Header.split(" ");
+
+        System.out.println("File name : "+Tokens[0]);
+        System.out.println("File size : "+Tokens[1]);
+
+        fobj = new File(Tokens[0]);
+       
+        fobj.createNewFile();
+
+        foobj = new FileOutputStream(fobj);
+
+        FileSize = Integer.parseInt(Tokens[1]);
+        
+        // Buffer for reading data
+        Buffer = new byte[FileSize];
+
+        // Read from packed file
+        fiobj.read(Buffer,0,FileSize);
+
+        // Decrypt the data
+        for(i=0; i<FileSize; i++)
+        {
+            Buffer[i] = (byte)(Buffer[i] ^ key);
+        }
+
+        //Write into extracted file
+        foobj.write(Buffer,0,FileSize);
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+      
+        fiobj.read(bHeader,0,100);
+
+        Header = new String(bHeader);
+
+        Header =  Header.trim();
+
+        Tokens = Header.split(" ");
+
+        System.out.println("File name : "+Tokens[0]);
+        System.out.println("File size : "+Tokens[1]);
+
+        fobj = new File(Tokens[0]);
+       
+        fobj.createNewFile();
+
+        foobj = new FileOutputStream(fobj);
+
+        FileSize = Integer.parseInt(Tokens[1]);
+        
+        // Buffer for reading data
+        Buffer = new byte[FileSize];
+
+        // Read from packed file
+        fiobj.read(Buffer,0,FileSize);
+
+        // Decrypt the data
+        for(i=0; i<FileSize; i++)
+        {
+            Buffer[i] = (byte)(Buffer[i] ^ key);
+        }
+
+        //Write into extracted file
+        foobj.write(Buffer,0,FileSize);
+        
+    }
+} 
+    
+
